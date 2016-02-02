b2df1d8ba4c9be12c29043c94f47c1e3698e1602
Ladislav Lhotka <lhotka@nic.cz>
2016-02-02 07:32:29 +0100

    Add search methods.

--- yangson/module/statement.py
+++ yangson/module/statement.py
@@ -21,16 +21,31 @@ class Statement(object):
                  arg: Optional[str],
                  sub: List["Statement"] = [],
                  pref: Optional[str] = None) -> None:
-        """Initialize the instance."""
+        """Initialize the instance.
+        """
         self.prefix = pref
         self.keyword = kw
         self.argument = arg
         self.substatements = sub
 
     def __str__(self) -> str:
+        """Return a string representation of the receiver.
+        """
         kw = (self.keyword if self.prefix is None
               else self.prefix + ":" + self.keyword)
         arg = ("" if self.argument is None
                else ' "' + self.argument.translate(self.escape_table) + '"')
         rest = " { ... }" if self.substatements else ";"
         return kw + arg + rest
+
+    def find1(self, kw: str, arg: str = None) -> Optional["Statement"]:
+        """Find the first substatement with the given keyword (and argument).
+        """
+        for sub in self.substatements:
+            if sub.keyword == kw and (arg is None or sub.argument == arg):
+                return sub
+
+    def find_all(self, kw: str) -> List["Statement"]:
+        """Find all substatements with the given keyword.
+        """
+        return [c for c in self.substatements if c.keyword == kw]
