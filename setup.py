8c2fb1b9fa3ef6161d7010f9b235f151ac09c5c3
Ladislav Lhotka <lhotka@nic.cz>
2016-02-04 13:54:37 +0100

    Add license and setup files.

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+def contents(*filenames):
+    buf = []
+    for filename in filenames:
+        with open(filename, encoding="utf-8") as fp:
+            buf.append(fp.read())
+    return "\n\n".join(buf)
+
+setup(
+    name = "yangson",
+    packages = ["yangson"],
+    version = "0.1.0",
+    description = "Library for working with YANG schemas and data",
+    author = "Ladislav Lhotka",
+    author_email = "lhotka@nic.cz",
+    url = "https://gitlab.labs.nic.cz/llhotka/yangson",
+    tests_require = ["pytest"],
+    keywords = ["yang", "data model", "configuration", "json"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Topic :: Software Development :: Libraries" ],
+    long_description = contents("README.rst")
+    )
