0e455f72289adc920e92fde787266fd10b3159aa
Ladislav Lhotka <lhotka@nic.cz>
2016-02-09 17:26:08 +0100

    Make RevisionDate type Optional.

--- yangson/types.py
+++ yangson/types.py
@@ -2,7 +2,7 @@ from typing import Optional, Tuple
 
 # Type aliases
 
-RevisionDate = str
+RevisionDate = Optional[str]
 Uri = str
 YangIdentifier = str
 ModuleId = Tuple[YangIdentifier, Optional[RevisionDate]]
