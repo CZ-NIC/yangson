ed8fa0c64f1598473f1bd9b6756f0b3a7578a422
Ladislav Lhotka <lhotka@nic.cz>
2016-02-19 16:43:59 +0100

    Update type alias.

--- yangson/typealiases.py
+++ yangson/typealiases.py
@@ -8,6 +8,6 @@ YangIdentifier = str
 NodeName = Tuple[YangIdentifier, YangIdentifier] # (namespace, name)
 SchemaAddress = List[NodeName]
 ModuleId = Tuple[YangIdentifier, Optional[RevisionDate]]
-Range = List[Tuple[Any, Any]]
+Range = List[List[Any]]
 QName = Tuple[YangIdentifier, YangIdentifier]
 PrefixMap = Dict[YangIdentifier, ModuleId]
