c79348f711404e1535b854384d7b38c136527674
Ladislav Lhotka <lhotka@nic.cz>
2016-06-07 09:13:08 +0200

    Write recursive descent parser for XPath.

--- yangson/xpath.py
+++ yangson/xpath.py
@@ -1,4 +1,4 @@
-from typing import Callable, List, Optional
+from typing import Any, Callable, List, Optional
 from .context import Context
 from .instance import InstanceNode
 from .parser import Parser, ParserException, EndOfInput
@@ -6,29 +6,46 @@ from .typealiases import *
 
 # Type aliases
 
-NodeExpr = Callable[[InstanceNode, InstanceNode], List[InstanceNode]]
-NodeTrans = Callable[[InstanceNode], List[InstanceNode]]
+NodeExpr = Callable[[InstanceNode], List[InstanceNode]]
 XPathExpr = Callable[[InstanceNode, InstanceNode], bool]
 
-class NodeSet:
-
-    def __init__(self, expr: NodeExpr) -> None:
-        self.eval = expr
+class NodeSet(list):
 
     @classmethod
-    def pure(cls, node = InstanceNode) -> "NodeSet":
-        return cls(lambda x, y: [node])
+    def pure(cls, node: InstanceNode) -> "NodeSet":
+        return cls([node])
 
-    def bind(self, f: NodeTrans) -> "NodeSet":
-        return NodeSet(lambda cur, orig:
-                       [ y for x in self.eval(cur, orig) for y in f(x) ])
+    @staticmethod
+    def union(nss: List[List[InstanceNode]]) -> List[InstanceNode]:
+        paths = set()
+        res = []
+        for ns in nss:
+            newp = set()
+            for i in ns:
+                p = i.path()
+                if p not in paths:
+                    newp.add(p)
+                    res.append(i)
+            paths |= newp
+        return res
 
+    def sort(self, reverse: bool = False):
+        super().sort(key=InstanceNode.path, reverse=reverse)
+
+    def bind(self, f: NodeExpr) -> "NodeSet":
+        return NodeSet(self.union([ f(x) for x in self ]))
+
+    def as_float(self) -> float:
+        return float(self[0].value)
+
+    def as_str(self) -> str:
+        return str(self[0].value)
+
+    def as_bool(self) -> bool:
+        return len(self) > 0
 
 class XPathParser(Parser):
-    """Parser for XPath expressions.
-
-    Method docstrings refer to production rules in XPath 1.0 grammar
-    """
+    """Parser for XPath expressions."""
 
     def __init__(self, mid: ModuleId) -> None:
         """Initialize the parser instance.
@@ -40,16 +57,157 @@ class XPathParser(Parser):
     def parse(self, xpath: str) -> XPathExpr:
         """Parse an XPath 1.0 expression."""
         super().parse(xpath)
-        start = NodeSet(lambda cur, orig: [cur])
-        return start.bind(self.step())
+        self.skip_ws()
+        return lambda node: self._or_expr(node, node)
 
-    def _resolve_step(self, axis, qn):
-        if axis == "child":
-            return lambda n: n.children(qn)
-        if axis == "ancestor-or-self":
-            return lambda n: n.ancestors_or_self(qn)
+    def _or_expr(self, cnode: InstanceNode, origin: InstanceNode):
+        op1 = self._and_expr(cnode, origin)
+        while self.test_string("or"):
+            self.skip_ws()
+            op2 = self._and_expr(cnode, origin)
+            op1 = op1 or op2
+        return op1
 
-    def step(self) -> NodeTrans:
+    def _and_expr(self, cnode: InstanceNode, origin: InstanceNode):
+        op1 = self._equality_expr(cnode, origin)
+        while self.test_string("and"):
+            self.skip_ws()
+            op2 = self._equality_expr(cnode, origin)
+            op1 = op1 and op2
+        return op1
+
+    def _equality_expr(self, cnode: InstanceNode, origin: InstanceNode):
+        op1 = self._relational_expr(cnode, origin)
+        while True:
+            negate = False
+            try:
+                next = self.peek()
+            except EndOfInput:
+                return op1
+            if next == "!":
+                self.offset += 1
+                negate = True
+                try:
+                    next = self.peek()
+                except EndOfInput:
+                    raise InvalidXPath(self, "EqualityExpr")
+            if next != "=":
+                if negate:
+                    raise InvalidXPath(self, "EqualityExpr")
+                return op1
+            self.adv_skip_ws()
+            op2 = self._relational_expr(cnode, origin)
+            op1 = op1 != op2 if negate else op1 == op2
+
+    def _relational_expr(self, cnode: InstanceNode, origin: InstanceNode):
+        op1 = self._additive_expr(cnode, origin)
+        while True:
+            try:
+                rel = self.peek()
+            except EndOfInput:
+                return op1
+            if rel not in "<>": return op1
+            self.offset += 1
+            eq = self.test_string("=")
+            self.skip_ws()
+            op2 = self._additive_expr(cnode, origin)
+            if rel == "<":
+                op1 = op1 <= op2 if eq else op1 < op2
+            else:
+                op1 = op1 >= op2 if eq else op1 > op2
+
+    def _additive_expr(self, cnode: InstanceNode, origin: InstanceNode):
+        op1 = self._multiplicative_expr(cnode, origin)
+        while True:
+            try:
+                pm = self.peek()
+            except EndOfInput:
+                return op1
+            if pm not in "+-": return op1
+            self.adv_skip_ws()
+            op2 = self._multiplicative_expr(cnode, origin)
+            op1 = op1 + op2 if pm == "+" else op1 - op2
+
+    def _multiplicative_expr(self, cnode: InstanceNode, origin: InstanceNode):
+        op1 = self._unary_expr(cnode, origin)
+        while True:
+            if self.test_string("*"):
+                mulop = 1
+            elif self.test_string("div"):
+                mulop = 2
+            elif self.test_string("mod"):
+                mulop = 3
+            else:
+                return op1
+            self.skip_ws()
+            op2 = self._unary_expr(cnode, origin)
+            if mulop == 1:
+                op1 *= op2
+            elif mulop == 2:
+                op1 /= op2
+            else:
+                op1 %= op2
+
+    def _unary_expr(self, cnode: InstanceNode, origin: InstanceNode):
+        negate = None
+        while self.test_string("-"):
+            negate = not negate
+        expr = self._primary_expr(cnode, origin)
+        if negate is None: return expr
+        if isinstance(expr, NodeSet):
+            return -expr.as_float() if negate else expr.as_float()
+        return -float(expr) if negate else float(expr)
+
+    def _primary_expr(self, cnode: InstanceNode, origin: InstanceNode):
+        next = self.peek()
+        if next == "(":
+            self.adv_skip_ws()
+            res = self._or_expr(cnode, origin)
+            self.char(")")
+            self.skip_ws()
+            return res
+        if next in "'\"":
+            self.offset += 1
+            res = self.up_to(next)
+            self.skip_ws()
+            return res
+        if next in "0123456789":
+            res = self.float()
+            self.skip_ws()
+            return res
+        if next == ".":
+            self.offset += 1
+            next = self.peek()
+            if next in "0123456789":
+                self.offset -= 1
+                res = self.float()
+                self.skip_ws()
+                return res
+        raise InvalidXPath(self, "PrimaryExpr")
+
+    def _resolve_step(self, axis: str, qn: QualName) -> NodeExpr:
+        try:
+            return { "child": lambda n: n.children(qn),
+                     "ancestor-or-self": lambda n: n.ancestors_or_self(qn),
+                     "descendant": lambda n: n.descendants(qn) }[axis]
+        except KeyError:
+            raise InvalidXPath(self, "AxisName")
+
+    def relative_location_path(self, cnode: InstanceNode, origin: InstanceNode):
+        """Parse ``RelativeLocationPath `` (production rule 3)."""
+        ns = NodeSet.pure(cnode)
+        while True:
+            ns = ns.bind(self.step())
+            self.skip_ws()
+            try:
+                next = self.peek()
+            except EndOfInput:
+                break
+            if next != "/": break
+            self.adv_skip_ws()
+        return ns
+
+    def step(self) -> NodeExpr:
         """Parse ``Step`` (production rule 4)."""
         next = self.peek()
         if next == "*":
@@ -57,13 +215,15 @@ class XPathParser(Parser):
             return self._resolve_step("child", None)
         ident = self.yang_identifier()
         ws = self.skip_ws()
-        next = self.peek()
+        try:
+            next = self.peek()
+        except EndOfInput:
+            return self._resolve_step("child", (ident, self.mid[0]))
         if next == ":":
             self.offset += 1
             next = self.peek()
             if next == ":":
-                self.offset += 1
-                self.skip_ws()
+                self.adv_skip_ws()
                 return self._resolve_step(ident, self.qname())
             if ws:
                 raise InvalidXPath(self, "Step")
