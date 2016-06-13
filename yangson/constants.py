19518a65eaada63aa5fbbe2d555d9371943de4d4
Ladislav Lhotka <lhotka@nic.cz>
2016-06-13 21:08:23 +0200

    Add enumeration of XPath axes.

--- yangson/constants.py
+++ yangson/constants.py
@@ -28,3 +28,22 @@ class DefaultDeny(Enum):
     none = 1
     write = 2
     all = 3
+
+class Axis(Enum):
+    """Enumeration of XPath axes."""
+    ancestor = 1
+    ancestor_or_self = 2
+    attribute = 3
+    child = 4
+    descendant = 5
+    descendant_or_self = 6
+    following_sibling = 7
+    parent = 8
+    preceding_sibling = 9
+    self = 10
+
+class MultiplicativeOp(Enum):
+    """Enumeration of XPath multiplicative operators."""
+    multiply = 1
+    divide = 2
+    modulo = 3
