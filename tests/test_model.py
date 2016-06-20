0923a44f911a6f76943628f7229d8d9c9fa800ed
Ladislav Lhotka <lhotka@nic.cz>
2016-06-20 11:37:48 +0200

    A few more pattern tests.

--- tests/test_model.py
+++ tests/test_model.py
@@ -402,6 +402,8 @@ def test_xpath(instance):
     xptest("string(round(0 div 0))", "NaN")
     xptest("re-match(//leafE, '[0-9a-fA-F]*')")
     xptest("re-match(count(//leafE), '[0-9]*')")
+    xptest("re-match('1.22.333', '\d{1,3}\.\d{1,3}\.\d{1,3}')")
+    xptest("re-match('aaax', 'a*')", False)
     xptest("re-match('a\nb', '.*')", False)
     xptest("re-match('a\nb', '[a-z\n]*')")
 
