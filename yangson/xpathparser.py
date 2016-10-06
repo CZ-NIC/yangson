32a58557623a1154f73a47c15d7ae6dee2b6e4c7
Ladislav Lhotka <lhotka@nic.cz>
2016-10-06 18:30:47 +0200

    Add copyright notices.

--- yangson/xpathparser.py
+++ yangson/xpathparser.py
@@ -1,3 +1,20 @@
+# Copyright © 2016 CZ.NIC, z. s. p. o.
+#
+# This file is part of Yangson.
+#
+# Yangson is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# Yangson is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License along
+# with Yangson.  If not, see <http://www.gnu.org/licenses/>.
+
 """Parser for XPath 1.0 expressions.
 
 This module defines the following classes:
