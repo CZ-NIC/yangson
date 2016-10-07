d654571378c6c12bb7909998f9fafbc4bee32a7d
Ladislav Lhotka <lhotka@nic.cz>
2016-10-07 21:20:59 +0200

    Update README.

--- README.rst
+++ README.rst
@@ -7,20 +7,32 @@ Welcome to Yangson
 :Author: Ladislav Lhotka <lhotka@nic.cz>
 :Date: |date|
 
-Yangson is a Python 3 library for working with `JSON encoded`_ configuration and state
-data modelled using the YANG_ data modelling language.
+*Yangson* is a Python 3 library for working with `JSON encoded`_
+configuration and state data modelled using the YANG_
+data modelling language.
 
 Installation
 ============
 
-Via pip TBD.
+::
+
+    python -m pip install yangson
+
+Note that *Yangson* requires Python 3.
+
+Development
+===========
+
+* Instructions_ for installing the development environment.
 
 Links
 =====
 
 * `Git repository`_
-* Documentation TBD
+* `Documentation`_
 
 .. _JSON encoded: https://tools.ietf.org/html/rfc7951
 .. _YANG: https://tools.ietf.org/html/rfc7950
+.. _Instructions: https://github.com/CZ-NIC/yangson/wiki/devinstall
 .. _Git repository: https://github.com/CZ-NIC/yangson
+.. _Documentation: http://yangson.readthedocs.io
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name = "yangson",
     packages = ["yangson"],
-    version = "1.0.0",
+    version = "1.0.0rc1",
     description = "Library for working with data modelled in YANG",
     author = "Ladislav Lhotka",
     author_email = "lhotka@nic.cz",
@@ -30,22 +30,28 @@ Welcome to Yangson
 :Author: Ladislav Lhotka <lhotka@nic.cz>
 :Date: |date|
 
-Yangson is a Python 3 library for working with `JSON encoded`_ configuration and state
-data modelled using the YANG_ data modelling language.
+*Yangson* is a Python 3 library for working with `JSON encoded`_
+configuration and state data modelled using the YANG_
+data modelling language.
 
 Installation
 ============
 
-Via pip TBD.
+::
+
+    python -m pip install yangson
+
+Note that *Yangson* requires Python 3.
 
 Links
 =====
 
 * `Git repository`_
-* Documentation TBD
+* `Documentation`_
 
 .. _JSON encoded: https://tools.ietf.org/html/rfc7951
 .. _YANG: https://tools.ietf.org/html/rfc7950
 .. _Git repository: https://github.com/CZ-NIC/yangson
+.. _Documentation: http://yangson.readthedocs.io
 """
     )
