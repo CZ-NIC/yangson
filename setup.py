31298581f8b6b8debfa5e0bf879b4ff5dbcb71d0
Ladislav Lhotka <lhotka@nic.cz>
2016-10-07 15:34:02 +0200

    Add MANIFEST.in, simplify setup.py.

--- setup.py
+++ setup.py
@@ -1,28 +1,37 @@
 from distutils.core import setup
 
-def contents(*filenames):
-    buf = []
-    for filename in filenames:
-        with open(filename, encoding="utf-8") as fp:
-            buf.append(fp.read())
-    return "\n\n".join(buf)
-
 setup(
     name = "yangson",
     packages = ["yangson"],
-    version = "0.1.53",
-    description = "Library for working with YANG schemas and data",
+    version = "1.0.0",
+    description = "Library for working with data modelled in YANG",
     author = "Ladislav Lhotka",
     author_email = "lhotka@nic.cz",
     url = "https://gitlab.labs.nic.cz/llhotka/yangson",
+    install_requires = ['PyXB'],
     tests_require = ["pytest"],
     keywords = ["yang", "data model", "configuration", "json"],
     classifiers = [
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
-        "Topic :: Software Development :: Libraries" ],
-    long_description = contents("README.rst")
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: System :: Systems Administration],
+    long_description = """\
+.. |date| date::
+
+=======
+Yangson
+=======
+:Author: Ladislav Lhotka <lhotka@nic.cz>
+:Date: |date|
+
+Python library for working with YANG_ data models and JSON-encoded
+data.
+
+.. _YANG: https://tools.ietf.org/html/draft-ietf-netmod-rfc6020bis
+"""
     )
