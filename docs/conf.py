be10113d47fadac5444c5d02cb7cc2f595914c74
Ladislav Lhotka <lhotka@nic.cz>
2016-11-18 21:42:43 +0100

    Update version in docs.

--- docs/conf.py
+++ docs/conf.py
@@ -60,7 +60,7 @@ author = 'Ladislav Lhotka'
 # built documents.
 #
 # The short X.Y version.
-version = '0.1'
+version = '1.1'
 # The full version, including alpha/beta/rc tags.
 release = '1.1.0'
 
