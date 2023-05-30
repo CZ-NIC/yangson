36711b6e862c9f867a09e8b0de892c2e616ffd14
Ladislav Lhotka <ladislav.lhotka@nic.cz>
2023-05-30 09:39:06 +0200

    Add skeleton for the ylnmda.py script

---
+++ tools/python/ylnmda.py
@@ -0,0 +1,20 @@
+"""This script reads the NMDA-aware version of YANG library [RFC8525]_
+and generates a corresponding old-style YANG library file that can be
+used with Yangson.
+
+Optionally, it can also download missing modules using the
+``location`` parameter (if available).
+
+"""
+
+import json
+import os
+import sys
+
+def main():
+    print("TBD")
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
