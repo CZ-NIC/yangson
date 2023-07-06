1d42dc9a711e56c9548b37f72b9b4f6ba01dad45
EckBalu <95221246+EckBalu@users.noreply.github.com>
2023-07-06 09:30:53 +0200

    Update exceptions.py
    Fix __str__ attribute of NotSupported. Wrong attribute call of the str of ParserException.

--- yangson/exceptions.py
+++ yangson/exceptions.py
@@ -211,7 +211,7 @@ class NotSupported(ParserException):
         self.feature = feature
 
     def __str__(self: "NotSupported") -> str:
-        return super().str() + ": " + str(self.feature)
+        return str(super()) + ": " + str(self.feature)
 
 
 class MissingModule(YangsonException):
